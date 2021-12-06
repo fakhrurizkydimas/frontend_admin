bdd25348bb4b1246c0fe5ef35c4cdf586b52da2f
enrichoalkalas01 <enrichoalkalas01@gmail.com>
2021-12-06 17:17:13 +0700

    update for update news pages

--- src/components/organisms/Edit/index.jsx
+++ src/components/organisms/Edit/index.jsx
@@ -5,6 +5,17 @@ import { useForm } from "react-hook-form";
 import axios from 'axios'
 import "./Add.scss";
 
+const readCookie = (name) => {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
 const Edit = () => {
   const [dateValue, setDateValue] = useState(new Date());
   const [dataSearch, setDataSearch] = useState(null)
@@ -25,10 +36,11 @@ const Edit = () => {
     formData.append('description', document.querySelector('#data-description').value)
     formData.append('date', dateValue)
     let config = {
-      url: "http://localhost:3031/viewnews", // TODO: update with correct API
-      method: "GET",
+      url: "http://localhost:3031/news/edit", // TODO: update with correct API
+      method: "post",
       headers: {
         "Content-Type": "application/json",
+        "Authorization": `Bearer ${ JSON.parse(readCookie('dataUser')).result.token }`
       },
       data: formData
     };
@@ -45,12 +57,11 @@ const Edit = () => {
   const SearchData = (e) => {
     if ( e.key === 'Enter' ) {
       let config = {
-        url: "http://localhost:3031/view/username", // TODO: update with correct API
-        method: "post",
+        url: `http://localhost:3031/viewnews/search?query=${ e.target.value }`, // TODO: update with correct API
+        method: "get",
         headers: {
           "Content-Type": "application/json",
-        },
-        data: { 'title': e.target.value }
+        }
       };
       
       axios(config)
@@ -69,22 +80,22 @@ const Edit = () => {
         <span className="title"></span>
 
         <div>
-          <input type="text" onKeyDown={ SearchData } />
-          <div>
+          <input type="text" onKeyDown={ SearchData } placeholder="Search data by title here.." style={{ width: '100%', padding: '10px 20px', borderRadius: '10px' }} />
+          <div style={{ height: '200px', overflowY: 'scroll', marginBottom: '25px', marginTop: '25px', boxShadow: '3px 3px 5px rgba(25,25,25,0.5)', padding: '1% 2.5%' }}>
             {
               dataSearch ?
                 dataSearch.map((data, i) => {
                   return(
-                    <div>
+                    <div key={ i }>
                       <hr />
-                      <div>title : { data.title }</div>
-                      <div>id : { data._id }</div>
+                      <div><b>title : { data.title }</b></div>
+                      <div style={{ fontSize: '20px' }}><b>id : { data._id }</b></div>
                       <div>excerpt : { data.excerpt }</div>
                       <hr />
                     </div>
                   )
                 })
-              : null
+              : <div style={{ padding: '2.5% 10px' }}>list of search data</div>
             }
           </div>
         </div>
@@ -133,7 +144,7 @@ const Edit = () => {
           <div className="mb-3 row">
             <label className="col-sm-2 col-form-label">Images</label>
             <div className="col-sm-6">
-              <input id="fileImages" type="file" accept="images/*" className="form-control input-custom" placeholder="Images" {...register("images")} name="images" />
+              <input id="fileImages" type="file" accept="image/*" className="form-control input-custom" placeholder="Images" {...register("images")} name="images" />
             </div>
           </div>
 
